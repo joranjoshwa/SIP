6052b8077e42ec4ceabb1533e3280020d11d5f4e
Joran Joshwa <joshwa315@gmail.com>
2025-08-26 01:07:18 -0300

    SIPF01/feat - integrando a tela com a api para verificacao do login do usuario

---
+++ app/src/context/AuthContext.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { createContext, ReactNode, useContext, useState } from "react";
+import { AuthContextType } from "../types/context";
+import { LoginPayload, LoginResponse } from "../types/auth";
+import { loginResponse } from "../api/endpoints/auth"
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+    const [user, setUser] = useState<LoginResponse | null> (null);
+
+    const login = async (payload: LoginPayload) => {
+        try {
+            const response = await loginResponse(payload);
+            setUser(response);
+
+            localStorage.setItem("token", response.token)
+
+        } catch (error) {
+            console.log("Erro no login: ", error);
+            throw error;
+        }
+    };
+
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("token");
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    
+    if (!context) throw new Error("useAuth deve ser usado dentro de AuthProvider");
+
+    return context;
+}
---
+++ app/src/types/auth.ts
@@ -0,0 +1,10 @@
+export type LoginPayload = {
+    email: string;
+    password: string;
+}
+
+export type LoginResponse = {
+    type: 'BEAR';
+    expiracao: string;
+    token: string;
+}
