f454196160d8b88eb8fc88e3dd122835e7d176cd
Joran Joshwa <joshwa315@gmail.com>
2025-08-29 00:34:24 -0300

    SIPF02/feat - adicionando os types do register e a logica do registro no auth context

--- app/src/api/endpoints/auth.ts
+++ app/src/api/endpoints/auth.ts
@@ -1,7 +1,12 @@
 import { api } from "../axios";
-import { LoginPayload, LoginResponse } from "../../types/auth"
+import { LoginPayload, LoginResponse, RegisterPayload } from "../../types/auth"
 
 export const loginResponse = async (payload: LoginPayload): Promise<LoginResponse> => {
     const { data } = await api.post("/authentication/login", payload);
+    return data;
+}
+
+export const registerResponse = async (payload: RegisterPayload): Promise<RegisterPayload> => {
+    const { data } = await api.post("/authentication/register", payload);
     return data;
 }
--- app/src/types/auth.ts
+++ app/src/types/auth.ts
@@ -8,3 +8,15 @@ export type LoginResponse = {
     expiracao: string;
     token: string;
 }
+
+export type RegisterPayload = {
+    name: string;
+    cpf: string;
+    email: string;
+    phone: string;
+    password: string;
+}
+
+export type RegisterResponse = {
+    message: string;
+}
