ae9aa0f696cb02396d14421d556f112f8cffd38c
Joran Joshwa <joshwa315@gmail.com>
2025-11-02 15:43:43 -0300

    SIPF01/refactor - ajustando as versoes da logo em white mode e dark mode

--- app/src/components/ui/Logo.tsx
+++ app/src/components/ui/Logo.tsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
+import logoInverted from "../../assets/logo-sip-dark.svg";
 import logo from "../../assets/logo-sip.svg"
+import logoBranca from "../../assets/logo-sip-branco.svg"
+import { useTheme } from "../../context/ThemeContext";
 
 type Props = {
     className?: string;
+    mode?: string;
+    imageClassName?: string;
 }
 
-export const Logo = ({ className }: Props) => {
+export const Logo = ({ className, mode = "light", imageClassName="" }: Props) => {
+
+    const { theme } = useTheme();
+    const logoSrc = theme === "dark" ? logoBranca : logo;
+
     return (
         <div className={`flex items-center justify-center ${className}`}>
-            <Image 
-                src={logo}
+            <Image
+                src={logoSrc}
                 alt="Logo SIP"
                 width={60}
                 height={90}
+                className={imageClassName}
                 priority
             />
         </div>
