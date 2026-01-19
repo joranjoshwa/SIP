64e58d204f86607c97a0e20a47350e203db49d90
Joran Joshwa <joshwa315@gmail.com>
2026-01-19 18:47:28 -0300

    alterando configuracoes para hml

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
+        protocol: "https",
         hostname: process.env.NEXT_PUBLIC_IMAGE_HOST!,
         port: process.env.NEXT_PUBLIC_IMAGE_PORT,
         pathname: "/**",
